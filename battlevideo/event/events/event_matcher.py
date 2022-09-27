905dc41ebb7855f75dc55b2f2bf22d8f338397a6
Go Yoshino <go.yoshino.2@gmail.com>
2022-09-27 15:49:20 +0900

    戻り値の型を変更

--- battlevideo/event/events/event_matcher.py
+++ battlevideo/event/events/event_matcher.py
@@ -1,10 +1,12 @@
 import abc
+from typing import Union, Tuple
 
+from battlevideo.event.events.event import Event
 from battlevideo.event.locale import Locale
 
 
 class EventMatcher(metaclass=abc.ABCMeta):
 
     @abc.abstractmethod
-    def matches(self, text: str, locale: Locale) -> bool:
+    def matches(self, text: str, locale: Locale) -> Tuple[bool, Union[int, None], Union[Event, None]]:
         pass
