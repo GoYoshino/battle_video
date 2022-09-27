5d221b80ae97f1e040d0d5bc7586f7a246763c59
Go Yoshino <go.yoshino.2@gmail.com>
2022-09-27 16:38:57 +0900

    reprを追加

--- battlevideo/battle_video.py
+++ battlevideo/battle_video.py
@@ -13,3 +13,9 @@ class BattleVideo:
 
     def __len__(self):
         return len(self.__events)
+
+    def __repr__(self):
+        repr = []
+        for event in self.__events:
+            repr.append(event.message)
+        return "\n".join(repr)
