75392d67f873c12e05c0d93c759e6a4f3c942ce0
Go Yoshino <go.yoshino.2@gmail.com>
2022-09-29 21:29:53 +0900

    繰り出しイベント（相手）のポケモン名正規化マッチングを実装

--- battlevideo/event/events/opponent_pokemon_sent.py
+++ battlevideo/event/events/opponent_pokemon_sent.py
@@ -1,28 +1,40 @@
-from typing import Tuple, List, Union
+from typing import Tuple, List, Union, Match
 import re
 
 from battlevideo.event.events.event import Event
 from battlevideo.event.events.event_matcher import EventMatcher
+from battlevideo.event.events.utils import remove_title
 from battlevideo.event.locale import Locale
+from ocr.normalization_ja import normalize
+from pokedex.pokedex import Pokedex
 
 
 class OpponentPokemonSentEvent(Event):
 
-    def __init__(self, message: str, pokemon: str):
+    def __init__(self, message: str, pokemon: str, meta_opponent_name: str):
         super().__init__(message)
         self.pokemon = pokemon
+        self.meta_opponent_name = meta_opponent_name
 
 
 class OpponentPokemonSentMatcher(EventMatcher):
 
+    def __init__(self, pokedex: Pokedex):
+        self.__pokedex = pokedex
+
     def matches(self, text: str, locale: Locale) -> Tuple[bool, Union[OpponentPokemonSentEvent, None]]:
-        match = re.search(".+は\n(.+)を くりだした!$", text)
+        match = re.search("(.+)は\n(.+)を くりだした!$", text)
         if match is not None:
-            pokemon = self.__get_pokemon_name(match.group(1))
-            return True, OpponentPokemonSentEvent(text, pokemon)
+            return True, self.__construct_event(match)
 
         return False, None
 
-    def __get_pokemon_name(self, string: str):
-        parts = string.split(" ")
-        return parts[len(parts) - 1]
+    def __construct_event(self, match: Match) -> OpponentPokemonSentEvent:
+        opponent_name = match.group(1)
+        pokemon_normalized = normalize(remove_title(match.group(2)))
+        pokemon = self.__pokedex.pokemons_reverse[pokemon_normalized]
+        message = self.__construct_message(pokemon, opponent_name)
+        return OpponentPokemonSentEvent(message, pokemon, opponent_name)
+
+    def __construct_message(self, pokemon: str, opponent_name: str):
+        return f"{opponent_name}は\n{pokemon}を くりだした!"
--- battlevideo/event/events/tests/test_opponent_pokemon_sent.py
+++ battlevideo/event/events/tests/test_opponent_pokemon_sent.py
@@ -5,20 +5,35 @@ from battlevideo.event.locale import Locale
 
 matcher = OpponentPokemonSentMatcher()
 
-class OpponentPokemonSentMatcherTest(TestCase):
 
-    def test_matches(self):
-        matches, event = matcher.matches("グリーンは\nザシアンを くりだした!", Locale.JA)
-        self.assertTrue(matches)
-        self.assertEqual("ザシアン", event.pokemon)
-        self.assertEqual("グリーンは\nザシアンを くりだした!", event.message)
+class OpponentPokemonSentMatcherTest(TestCase):
 
     def test_other_message_does_not_match(self):
         matches, event = matcher.matches("ウオノラゴンは\nゴツゴツメットで ダメージを受けた!", Locale.JA)
         self.assertFalse(matches)
 
+    def test_works_with_ordinary_case(self):
+        matches, event = matcher.matches("グリーンは\nザシアンを くりだした!", Locale.JA)
+        self.assertTrue(matches)
+        self.assertEqual("ザシアン", event.pokemon)
+        self.assertEqual("グリーンは\nザシアンを くりだした!", event.message)
+
+    def test_does_normalized_match_for_pokemon_name(self):
+        matches, event = matcher.matches("グリーンは\nウソツキーを くりだした!", Locale.JA)
+        self.assertTrue(matches)
+
+    def test_revises_pokemon_name(self):
+        matches, event = matcher.matches("グリーンは\nウソッキーを くりだした!", Locale.JA)
+        self.assertTrue(matches)
+        self.assertEqual("ウソツキー", event.pokemon)
+
+    def test_does_not_normalize_opponent_name(self):
+        matches, event = matcher.matches("おちゃは\nザシアンを くりだした!", Locale.JA)
+        self.assertTrue(matches)
+        self.assertEqual("おちゃは\nザシアンを くりだした!", event.message)
+
     def test_works_with_titled_pokemon(self):
         matches, event = matcher.matches("グリーンは\nランクマスター ザシアンを くりだした!", Locale.JA)
         self.assertTrue(matches)
         self.assertEqual("ザシアン", event.pokemon)
-        self.assertEqual("グリーンは\nランクマスター ザシアンを くりだした!", event.message)
+        self.assertEqual("グリーンは\nザシアンを くりだした!", event.message)
