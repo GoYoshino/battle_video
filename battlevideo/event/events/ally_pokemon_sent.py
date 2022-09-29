416353ebd5ca0ae3c3d2ed5beb55aae17f5aeb4f
Go Yoshino <go.yoshino.2@gmail.com>
2022-09-29 21:13:11 +0900

    ポケモン繰り出しイベント（味方）の正規化マッチングを実装

--- battlevideo/event/events/ally_pokemon_sent.py
+++ battlevideo/event/events/ally_pokemon_sent.py
@@ -1,9 +1,11 @@
-from typing import Tuple, List, Union
-import re
+from enum import Enum
+from typing import Tuple, List, Union, Match
 
 from battlevideo.event.events.event import Event
 from battlevideo.event.events.event_matcher import EventMatcher
+from battlevideo.event.events.normalized_match import normalized_search
 from battlevideo.event.locale import Locale
+from pokedex.pokedex import Pokedex
 
 
 class AllyPokemonSentEvent(Event):
@@ -13,26 +15,58 @@ class AllyPokemonSentEvent(Event):
         self.pokemon = pokemon
 
 
+class AllyPokemonSentMessageType(Enum):
+    GO = 1
+    IM_COUNTING_ON_YOU = 2
+    TAKE_YOUR_CHANCE = 3
+
 class AllyPokemonSentMatcher(EventMatcher):
 
+    def __init__(self, pokedex: Pokedex):
+        self.__pokedex = pokedex
+
     def matches(self, text: str, locale: Locale) -> Tuple[bool, Union[AllyPokemonSentEvent, None]]:
-        match = re.search("ゆけっ!\s(.+)!$", text)
+        match = normalized_search("ゆけつ!\s(.+)!$", text)
         if match is not None:
-            pokemon = self.__get_pokemon_name(match.group(1))
-            return True, AllyPokemonSentEvent(text, pokemon)
+            event = self.__construct_event(match, AllyPokemonSentMessageType.GO)
+            return True, event
 
-        match = re.search("相手が\s弱っている!\nチャンスだ!\s(.+)!$", text)
+        match = normalized_search("任せた!\s(.+)!$", text)
         if match is not None:
-            pokemon = self.__get_pokemon_name(match.group(1))
-            return True, AllyPokemonSentEvent(text, pokemon)
+            event = self.__construct_event(match, AllyPokemonSentMessageType.IM_COUNTING_ON_YOU)
+            return True, event
 
-        match = re.search("任せた!\s(.+)!$", text)
+        match = normalized_search("相手が\s弱つている!\nチヤンスだ!\s(.+)!$", text)
         if match is not None:
-            pokemon = self.__get_pokemon_name(match.group(1))
-            return True, AllyPokemonSentEvent(text, pokemon)
+            event = self.__construct_event(match, AllyPokemonSentMessageType.TAKE_YOUR_CHANCE)
+            return True, event
 
         return False, None
 
     def __get_pokemon_name(self, string: str):
         parts = string.split(" ")
         return parts[len(parts) - 1]
+
+    def __construct_event(self, match: Match, message_type: AllyPokemonSentMessageType) -> AllyPokemonSentEvent:
+        pokemon = remove_title(match.group(1))
+        pokemon = self.__pokedex.pokemons_reverse[pokemon]
+        # TODO: 逆引き辞書にヒットしなかったパターンの処理を考える
+        message = self.__construct_message(message_type, pokemon)
+
+        return AllyPokemonSentEvent(message, pokemon)
+
+    def __construct_message(self, message_type: AllyPokemonSentMessageType, pokemon: str) -> str:
+        if message_type == AllyPokemonSentMessageType.GO:
+            return f"ゆけっ! {pokemon}!"
+        elif message_type == AllyPokemonSentMessageType.IM_COUNTING_ON_YOU:
+            return f"任せた! {pokemon}!"
+        elif message_type == AllyPokemonSentMessageType.TAKE_YOUR_CHANCE:
+            return f"相手が 弱っている!\nチャンスだ! {pokemon}!"
+        raise NotImplementedError()
+
+def remove_title(pokemon_text: str) -> str:
+    if " " not in pokemon_text:
+        return pokemon_text
+
+    # 称号は捨てる　かなしいね・・
+    return pokemon_text.split(" ")[1]
--- battlevideo/event/events/tests/test_ally_pokemon_sent.py
+++ battlevideo/event/events/tests/test_ally_pokemon_sent.py
@@ -2,47 +2,148 @@ from unittest import TestCase
 
 from battlevideo.event.events.ally_pokemon_sent import AllyPokemonSentMatcher
 from battlevideo.event.locale import Locale
+from pokedex.pokedex import Pokedex
 
-matcher = AllyPokemonSentMatcher()
+matcher = AllyPokemonSentMatcher(Pokedex())
 
 class AllyPokemonSentMatcherTest(TestCase):
-
-    def test_yuke_matches(self):
-        matches, event = matcher.matches("ゆけっ! ウオノラゴン!", Locale.JA)
-        self.assertTrue(matches)
-        self.assertEqual("ウオノラゴン", event.pokemon)
-        self.assertEqual("ゆけっ! ウオノラゴン!", event.message)
 
     def test_other_message_does_not_match(self):
         matches, event = matcher.matches("ウオノラゴンは\nゴツゴツメットで ダメージを受けた!", Locale.JA)
         self.assertFalse(matches)
 
-    def test_makaseta_matches(self):
+    def test_type1_matches(self):
+        matches, event = matcher.matches("ゆけっ! ウオノラゴン!", Locale.JA)
+        self.assertTrue(matches)
+        self.assertEqual("ウオノラゴン", event.pokemon)
+        self.assertEqual("ゆけっ! ウオノラゴン!", event.message)
+
+    def test_type1_works_with_titled_pokemon(self):
+        matches, event = matcher.matches("ゆけっ! ガラルチャンピオン ウオノラゴン!", Locale.JA)
+        self.assertTrue(matches)
+        self.assertEqual("ウオノラゴン", event.pokemon)
+        self.assertEqual("ゆけっ! ウオノラゴン!", event.message)
+
+    def test_type2_matches(self):
         matches, event = matcher.matches("任せた! ウオノラゴン!", Locale.JA)
         self.assertTrue(matches)
         self.assertEqual("ウオノラゴン", event.pokemon)
         self.assertEqual("任せた! ウオノラゴン!", event.message)
 
-    def test_yowatteiru_matches(self):
+    def test_type2_works_with_titled_pokemon(self):
+        matches, event = matcher.matches("任せた! ガラルチャンピオン ウオノラゴン!", Locale.JA)
+        self.assertTrue(matches)
+        self.assertEqual("ウオノラゴン", event.pokemon)
+        self.assertEqual("任せた! ウオノラゴン!", event.message)
+
+    def test_type3_matches(self):
         matches, event = matcher.matches("相手が 弱っている!\nチャンスだ! ウオノラゴン!", Locale.JA)
         self.assertTrue(matches)
         self.assertEqual("ウオノラゴン", event.pokemon)
         self.assertEqual("相手が 弱っている!\nチャンスだ! ウオノラゴン!", event.message)
 
-    def test_works_with_titled_pokemon_yuke(self):
-        matches, event = matcher.matches("ゆけっ! ガラルチャンピオン ウオノラゴン!", Locale.JA)
-        self.assertTrue(matches)
-        self.assertEqual("ウオノラゴン", event.pokemon)
-        self.assertEqual("ゆけっ! ガラルチャンピオン ウオノラゴン!", event.message)
-
-    def test_works_with_titled_pokemon_makaseta(self):
-        matches, event = matcher.matches("任せた! ガラルチャンピオン ウオノラゴン!", Locale.JA)
-        self.assertTrue(matches)
-        self.assertEqual("ウオノラゴン", event.pokemon)
-        self.assertEqual("任せた! ガラルチャンピオン ウオノラゴン!", event.message)
-
-    def test_works_with_titled_pokemon_yowatteiru(self):
+    def test_type3_works_with_titled_pokemon(self):
         matches, event = matcher.matches("相手が 弱っている!\nチャンスだ! ガラルチャンピオン ウオノラゴン!", Locale.JA)
         self.assertTrue(matches)
         self.assertEqual("ウオノラゴン", event.pokemon)
-        self.assertEqual("相手が 弱っている!\nチャンスだ! ガラルチャンピオン ウオノラゴン!", event.message)
+        self.assertEqual("相手が 弱っている!\nチャンスだ! ウオノラゴン!", event.message)
+
+    ### normalization ###
+    def test_type1_does_normalized_match_for_text_base(self):
+        matches, event = matcher.matches("ゆけつ! ウオノラゴン!", Locale.JA)
+        self.assertTrue(matches)
+
+    def test_type1_revises_text_base(self):
+        matches, event = matcher.matches("ゆけつ! ウオノラゴン!", Locale.JA)
+        self.assertTrue(matches)
+        self.assertEqual("ゆけっ! ウオノラゴン!", event.message)
+
+    def test_type1_does_normalized_match_for_pokemon_name(self):
+        matches, event = matcher.matches("ゆけつ! モジヤンボ!", Locale.JA)
+        self.assertTrue(matches)
+
+    def test_type1_revises_pokemon_name(self):
+        matches, event = matcher.matches("ゆけつ! モジヤンボ!", Locale.JA)
+        self.assertEqual("ゆけっ! モジャンボ!", event.message)
+
+    def test_type2_does_normalized_match_for_pokemon_name(self):
+        matches, event = matcher.matches("任せた! モジヤンボ!", Locale.JA)
+        self.assertTrue(matches)
+
+    def test_type2_revises_pokemon_name(self):
+        matches, event = matcher.matches("任せた! モジヤンボ!", Locale.JA)
+        self.assertEqual("任せた! モジャンボ!", event.message)
+
+    def test_type3_does_normalized_match_for_text_base_former(self):
+        matches, event = matcher.matches("相手が 弱つている!\nチャンスだ! ウオノラゴン!", Locale.JA)
+        self.assertTrue(matches)
+        self.assertEqual("ウオノラゴン", event.pokemon)
+
+    def test_type3_revises_text_base_former(self):
+        matches, event = matcher.matches("相手が 弱つている!\nチャンスだ! ウオノラゴン!", Locale.JA)
+        self.assertEqual("相手が 弱っている!\nチャンスだ! ウオノラゴン!", event.message)
+
+    def test_type3_does_normalized_match_for_text_base_latter(self):
+        matches, event = matcher.matches("相手が 弱っている!\nチヤンスだ! ウオノラゴン!", Locale.JA)
+        self.assertTrue(matches)
+        self.assertEqual("ウオノラゴン", event.pokemon)
+
+    def test_type3_revises_text_base_latter(self):
+        matches, event = matcher.matches("相手が 弱っている!\nチヤンスだ! ウオノラゴン!", Locale.JA)
+        self.assertEqual("相手が 弱っている!\nチャンスだ! ウオノラゴン!", event.message)
+
+    def test_type3_does_normalized_match_for_text_base_both(self):
+        matches, event = matcher.matches("相手が 弱つている!\nチヤンスだ! ウオノラゴン!", Locale.JA)
+        self.assertTrue(matches)
+        self.assertEqual("ウオノラゴン", event.pokemon)
+
+    def test_type3_revises_text_base_both(self):
+        matches, event = matcher.matches("相手が 弱つている!\nチヤンスだ! ウオノラゴン!", Locale.JA)
+        self.assertEqual("相手が 弱っている!\nチャンスだ! ウオノラゴン!", event.message)
+
+    def test_type3_does_normalized_match_for_pokemon_name(self):
+        matches, event = matcher.matches("相手が 弱つている!\nチャンスだ! モジヤンボ!", Locale.JA)
+        self.assertTrue(matches)
+
+    def test_type3_revises_pokemon_name(self):
+        matches, event = matcher.matches("相手が 弱つている!\nチャンスだ! モジヤンボ!", Locale.JA)
+        self.assertEqual("相手が 弱っている!\nチャンスだ! モジャンボ!", event.message)
+
+
+    ### ビックリマーク関連: 仕様にするか未定。実際に上がってくるOCRデータを見て決めたい ###
+    def test_type1_tolerates_lost_exclamation_mark_former(self):
+        pass
+
+    def test_type1_tolerates_lost_exclamation_mark_latter(self):
+        pass
+
+    def test_type1_tolerates_lost_exclamation_mark_both(self):
+        pass
+
+    def test_type1_revises_lost_exclamation_mark(self):
+        pass
+
+    def test_type2_tolerates_lost_exclamation_mark_former(self):
+        pass
+
+    def test_type2_tolerates_lost_exclamation_mark_latter(self):
+        pass
+
+    def test_type2_tolerates_lost_exclamation_mark_both(self):
+        pass
+
+    def test_type2_revises_lost_exclamation_mark(self):
+        pass
+
+    def test_type3_tolerates_lost_exclamation_mark_former(self):
+        pass
+
+    def test_type3_tolerates_lost_exclamation_mark_latter(self):
+        pass
+
+    def test_type3_tolerates_lost_exclamation_mark_both(self):
+        pass
+
+    def test_type3_revises_lost_exclamation_mark(self):
+        pass
+
